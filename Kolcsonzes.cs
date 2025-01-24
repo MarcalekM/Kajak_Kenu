d2540a8378205ecdd51dae4af60c8bbe0565f6be
Sufi <120452154+MarcalekM@users.noreply.github.com>
2025-01-24 11:31:55 +0100

    Add project files.

---
+++ Kolcsonzes.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kajak_Kenu
+{
+    internal class Kolcsonzes
+    {
+        public string Nev { get; set; }
+        public int HajoAzonosito { get; set; }
+        public string HajoTipusa { get; set; }
+        public int SzemelyekSzama { get; set; }
+        public int ElvitelOra { get; set; }
+        public int ElvitelPerc { get; set; }
+        public int VisszahozatalOra { get; set; }
+        public int VisszahozatalPerc { get; set; }
+
+        public Kolcsonzes(string sor)
+        {
+            var temp = sor.Split(';').ToList();
+            Nev = temp[0];
+            HajoAzonosito = int.Parse(temp[1]);
+            HajoTipusa = temp[2];
+            SzemelyekSzama = int.Parse(temp[3]);
+            ElvitelOra = int.Parse(temp[4]);
+            ElvitelPerc = int.Parse(temp[5]);
+            VisszahozatalOra = int.Parse(temp[6]);
+            VisszahozatalPerc = int.Parse(temp[7]);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using Kajak_Kenu;
+
+List<Kolcsonzes> kolcsonzesek = new();
+string path = @"../../../src";
+
+using StreamReader sr = new(
+    path: $"{path}/kolcsonzes.txt",
+    encoding: System.Text.Encoding.UTF8);
+_ = sr.ReadLine();
+while (!sr.EndOfStream) kolcsonzesek.Add(new(sr.ReadLine()));
+
+Console.WriteLine($"4. feladat:\nÖsszesen {kolcsonzesek.Count} db kölcsönzés adatai találhatók a forrásban");
+
+Console.WriteLine("\n5. feladat:\nAdja meg a keresett személyt:");
+string keresett = Console.ReadLine();
+var keresettKolcsonzes = kolcsonzesek.Where(k => k.Nev.Contains(keresett.Replace(",", ""))).ToList();
+foreach (var item in keresettKolcsonzes)
+{
+    Console.WriteLine($"");
+}
+
+
+Console.WriteLine("\n6. feladat:");
+var magyarok = kolcsonzesek.Where(k => !k.Nev.Contains(",")).ToList().Count();
+var kulfoldi = kolcsonzesek.Where(k => k.Nev.Contains(",")).ToList().Count();
+Console.WriteLine($"A magyar kölcsönzők száma: {magyarok}\na külföldieké: {kulfoldi}");
